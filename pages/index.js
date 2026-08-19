2e34640d9e7bc3479156829204125523a9bc2158
antoninaparulava-coder <antoninaparulava@gmail.com>
2026-08-19 20:47:18 +0400

    add starting page files

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import StartingPageContent from '../components/starting-page/starting-page';
+
 function HomePage() {
-  return <h1>Just a basic page!</h1>;
+  return <StartingPageContent />;
 }
 
 export default HomePage;
