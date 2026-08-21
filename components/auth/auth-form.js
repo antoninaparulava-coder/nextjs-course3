2784502e7943b786f1be170754170a170954bed8
antoninaparulava-coder <antoninaparulava@gmail.com>
2026-08-21 21:08:46 +0400

    Sending a Signin Request From The Frontend

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from 'react';
 import classes from './auth-form.module.css';
+import { signIn } from 'next-auth/react';
 
 async function createUser(email, password){
    const response = await fetch('/api/auth/signup', {
@@ -36,7 +37,15 @@ function AuthForm() {
     const enteredPassword = passwordInputRef.current.value;
 
     if(isLogin){
+      const result = await signIn('credentials', { 
+        redirect: false,
+        email: enteredEmail,
+        password: enteredPassword
+      });
 
+      if(!result.error){
+        // set some autj state
+      }
     }else{
       try{
         const result = await createUser(enteredEmail, enteredPassword);
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,14 @@
 import { verifyPassword } from "@/lib/auth";
 import { connectToDatabase } from "@/lib/db";
 import NextAuth from "next-auth";
-import Providers from 'next-auth/providers'
+import CredentialsProvider from 'next-auth/providers/credentials'
 
 export default NextAuth({
     session: {
         jwt: true
     },
     providers: [
-        Providers.Credentials({
+        CredentialsProvider({
             async authorize(credentials) {
                 const client = await connectToDatabase();
 
